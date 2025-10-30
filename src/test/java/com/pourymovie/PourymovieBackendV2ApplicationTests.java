267a8bf6978bd83ebcc75b616eacf1063f8a8e4d
Pourya Alipanah <pourya.alipanah2@gmail.com>
2025-10-30 19:22:26 +0330

    Initializes the project structure
    Sets up the basic project structure with necessary files such as .gitignore, .gitattributes, pom.xml, compose.yaml, maven wrapper and application properties.
    This provides a foundation for further development.

---
+++ src/test/java/com/pourymovie/PourymovieBackendV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pourymovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PourymovieBackendV2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
