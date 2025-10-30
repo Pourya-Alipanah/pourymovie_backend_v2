a09cb89ce67cefefc928c5a8688aaac84ea3c814
Pourya Alipanah <pourya.alipanah2@gmail.com>
2025-10-30 21:25:54 +0330

    Configures basic project structure
    Initializes the project with basic configuration, including:
    
    - Adds default configurations for application properties.
    - Sets up Swagger for API documentation.
    - Configures global validation.
    - Configures default page size and number.
    - Creates basic security configuration.
    - Adds .gitkeep files to controller, dto, entity, exception, repository, service, and util packages.
    - Adds .env to .gitignore
    - Updates springdoc version and configuration.

---
+++ src/main/java/com/pourymovie/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.pourymovie.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+            .info(new Info()
+                    .title("PouryMovie API")
+                    .description("Use the base API URL as http://localhost:1406/api/v2 just for local development")
+                    .version("2.0"))
+            .addServersItem(new Server().url("http://localhost:1406/api/v2"))
+            .addServersItem(new Server().url("https://api.pourymovie.ir/api/v2"))
+            .schemaRequirement("access-token", new SecurityScheme()
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT"))
+            .addSecurityItem(new SecurityRequirement().addList("access-token"));
+  }
+}
